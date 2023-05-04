4225fe9d70d3ca97ef82e4c5a2204408da1172c6
Felix Mckenzie <felixmckenzie90@gmail.com>
2023-05-04 18:14:41 +1000

    add config directory for routes

---
+++ src/config/MainNavConfig.ts
@@ -0,0 +1,21 @@
+
+
+export const NavConfig = {
+    mainNav: [
+        {
+            title: 'Messages',
+            href: '/messages',
+            disabled: false,
+        },
+        {
+            title: 'Contacts',
+            href: '/contacts',
+            disabled: false,
+        },
+        {
+            title: 'Friend Requests',
+            href: '/friend-requests',
+            disabled: false,
+        },
+    ],
+}
