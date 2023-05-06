bace16f2585ee9b974fabca868efad683a610c4b
Felix Mckenzie <felixmckenzie90@gmail.com>
2023-05-06 17:02:14 +1000

    wrap App with theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from './providers/themeProvider'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { ApolloProviderWrapper } from './providers/apolloProvider'
 const clerk_pub_key = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -10,10 +11,12 @@ const clerk_pub_key = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <ClerkProvider publishableKey={clerk_pub_key}>
-      <ApolloProviderWrapper>
+    <ThemeProvider>
+      <ClerkProvider publishableKey={clerk_pub_key}>
+        <ApolloProviderWrapper>
         <App/>
-      </ApolloProviderWrapper>
-    </ClerkProvider>
+        </ApolloProviderWrapper>
+      </ClerkProvider>
+    </ThemeProvider>
   </BrowserRouter>
 )
