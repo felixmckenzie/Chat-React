e2e973fa6866472a2ec9ae33206ff53594bed55e
Felix Mckenzie <felixmckenzie90@gmail.com>
2023-05-06 18:20:43 +1000

    add theme palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,tsx,ts}"],
+  darkMode: "class", // Set darkMode to 'class' for manual toggling
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        light: "#F5F5F5",
+        dark: "#0f0e17",
+        highlight: "#ff8906",
+      },
+      textColor: {
+        dark: "#2e2f3e",
+        light: "#fffffe",
+      },
+    },
   },
   plugins: [],
 };
