b5c5f3950b09a1ca277c50e1ca1589d2be3392a3
Felix Mckenzie <felixmckenzie90@gmail.com>
2023-05-06 17:00:46 +1000

    implement theme provider

---
+++ src/providers/themeProvider.tsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from 'react';
+
+const ThemeContext = createContext(null);
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <ThemeContext.Provider value={{theme,toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+
+};
