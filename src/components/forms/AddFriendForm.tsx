9c061790fad97bc891a58d7958ac8e2d01f19adc
Felix Mckenzie <felixmckenzie90@gmail.com>
2023-05-09 09:53:44 +1000

    fix onSubmit function

--- src/components/forms/AddFriendForm.tsx
+++ src/components/forms/AddFriendForm.tsx
@@ -25,7 +25,7 @@ export const AddFriendForm: FC = () => {
             toast.success(`Friend Request Sent to ${receiverUsername}`)
         },
         onError: (e) => {
-            if (e && friendRequestError) {
+            if (friendRequestError) {
                 toast.error(friendRequestError?.message)
             } else {
                 toast.error('Failed to Send! Please Try Again Later')
@@ -44,7 +44,7 @@ export const AddFriendForm: FC = () => {
     return (
         <div className="w-1/2">
             <form onSubmit={handleSubmit(onSubmit)}>
-                <Input label="CaSe SeNsItIvE" name="addFriend" placeholder="Enter Email Address" register={register}>
+                <Input label="CaSe SeNsItIvE" name="contactUserEmail" placeholder="Enter Email Address" register={register}>
                     <div className="absolute inset-y-0 right-0 flex items-center px-2">
                         <button type="submit" className="rounded p-2 bg-highlight text-light text-xs">
                             Send Friend Request
