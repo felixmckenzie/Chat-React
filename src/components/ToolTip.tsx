f1c4633c176d7cf56733bfc1a1827ca154e9c2e1
Felix Mckenzie <felixmckenzie90@gmail.com>
2023-05-09 21:44:34 +1000

    format tooltip

--- src/components/ToolTip.tsx
+++ src/components/ToolTip.tsx
@@ -12,7 +12,7 @@ export const Tooltip: React.FC<TooltipProps> = ({ children, content }) => {
     return (
         <div className="relative inline-block cursor-pointer" onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)}>
             {children}
-            {show && <div className="absolute px-2 py-1 mt-2 text-xs bg-body-text-light text-headline-text rounded-md shadow-lg">{content}</div>}
+            {show && <div className="absolute px-2 py-2 mt-4 text-xs rounded-md shadow-lg">{content}</div>}
         </div>
     )
 }
