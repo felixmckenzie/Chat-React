c48a0c0de80e3cca4b513585adff799ae485ba5a
Felix Mckenzie <felixmckenzie90@gmail.com>
2023-05-12 10:24:51 +1000

    Change component name from MessageItem to DetailCard

--- src/components/DetailCard.tsx
+++ src/components/DetailCard.tsx
@@ -1,19 +1,26 @@
 import { FormatDate } from '../utils'
 import { Avatar } from './Avartar'
 
+interface DetailCardProps {
+    title: string
+    text: string
+    avatar?: string
+    date?: string
+}
 
-export const MessageItem = ({ title, text, date }: { title: string; text: string; date?: string }) => {
+
+export const DetailCard = ({ title, text, date, avatar }: DetailCardProps) => {
     return (
-        <div className="flex w-full p-2 text-body-text-light">
+        <div className="flex w-full p-2 border-b">
             <div className="flex flex-shrink items-center">
-                <Avatar width={20} height={20} alt="avatar" avatarUrl={''} />
+                <Avatar width={20} height={20} alt="avatar" avatarUrl={avatar} />
             </div>
             <div className="flex flex-grow overflow-y-hidden flex-col ml-4">
                 <div className="flex justify-between items-center">
                     <h1>{title}</h1>
                     {date && <span className="text-right text-xs truncate ...">{FormatDate(date)}</span>}
                 </div>
-                <p className="truncate ...">{text}</p>
+                <p className="text-xs truncate ...">{text}</p>
             </div>
         </div>
     )
