7726a752f17f5f92a9bbac6df6821ad0a90dfcdd
Felix Mckenzie <felixmckenzie90@gmail.com>
2023-05-21 11:57:37 +1000

    add messages

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,6 +1,11 @@
 import friendRequest from "./friendRequest";
 import user from "./user";
+import chat from "./chat";
+import message from "./message";
+
 export default {
   ...user,
   ...friendRequest,
+  ...chat,
+  ...message,
 };
